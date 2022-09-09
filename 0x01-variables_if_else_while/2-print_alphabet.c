d500b19310f9dc27eed019c8f87628e8ca296fa7
Macaire <macaire.ngue@outlook.com>
2022-09-09 14:20:44 -0700

    prints the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * main - Enrty point
- * Description: a program that prints the alphabet in lowercase, followed by a new line.
+ * Description: a program that prints the alphabet in lowercase.
  * Return: Always 0 (Success)
  */
 int main(void)
