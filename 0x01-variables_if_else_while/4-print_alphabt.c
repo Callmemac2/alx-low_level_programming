040bc667f4eeb5ece4752147f442792dacb788d7
Macaire <macaire.ngue@outlook.com>
2022-09-09 15:32:50 -0700

    Alphabet in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,11 +7,17 @@
 int main(void)
 {
 	int ch;
-	for (ch = 'a'; ch <= 'z'; ch++)
-		if (ch != 'e' && ch != 'q')
-			putchar(ch);
-		else
 
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		else
+		{
+		}
+	}
 	putchar('\n');
 	return (0);
 }
