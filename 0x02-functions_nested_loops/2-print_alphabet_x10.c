fa875b741f040e030e16eaf8b1f6d183b632e0c5
Macaire <macaire.ngue@outlook.com>
2022-09-13 11:45:22 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,14 @@ void print_alphabet_x10(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
+	{
 
 		char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
-	_putchar('\n');
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{	
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
 }
