915f2402f6049b991149c9b6f37bfefcfee13d57
Macaire <macaire.ngue@outlook.com>
2022-09-15 12:43:43 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers from 0 to 9
+ *
+ * Return : Always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 'O'; i <= '9'; i++)
+	{
+		_putchar(i);
+
+	}
+	_putchar('\n');
+}
