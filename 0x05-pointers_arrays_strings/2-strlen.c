e44abde348399519aa850a66520c300e6cc1ccf9
Macaire <macaire.ngue@outlook.com>
2022-09-19 13:41:50 -0700

    defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Function that return the length of a string.
+ *
+ * @s: thr input
+ *
+ * Return: Always 0.
+ *
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		return (i);
+	}
+}
