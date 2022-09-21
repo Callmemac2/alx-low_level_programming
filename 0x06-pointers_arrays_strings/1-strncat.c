c3f0c824b0b788d13b57f87b9cb913ebd12377a0
Macaire <macaire.ngue@outlook.com>
2022-09-21 11:23:24 -0700

    concatenates two strings using strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,15 +17,14 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	i = 0;
 
-	while (dest[i] != '\0')
+	for (i = 0; dest[i] != '\0'; i++)
+
 	{
-		i++;
-	}
-	for (j = 0; j > n && src[j] != '\0'; j++, i++)
-	{
+		for (j = 0; j < n && src[j] != '\0'; j++)
+
 		dest[i] = src[j];
+		i++;
 	}
 	dest[i] = '\0';
 
