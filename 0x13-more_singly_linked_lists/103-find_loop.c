53e4c84b829de41b85aa41194af4a84cab312815
Macaire <macaire.ngue@outlook.com>
2022-10-25 07:51:29 -0700

    Find the loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -21,7 +21,7 @@ listint_t *find_listint_loop(listint_t *head)
 		if (node1 == node2)
 		{
 			node1 = head;
-			
+
 			while (node1 != node2)
 			{
 				node1 = node1->next;
@@ -30,10 +30,10 @@ listint_t *find_listint_loop(listint_t *head)
 
 			return (node1);
 		}
-		
+
 		node1 = node1->next;
 		node2 = (node2->next)->next;
 	}
-	
+
 	return (NULL);
 }
